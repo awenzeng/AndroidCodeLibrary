9b365d56061db6a1530c2d809c4264512eeb8b1b
AwenZeng <awenyini@gmail.com>
2019-03-28 17:21:12 +0800

    闪屏页添加
    Change-Id: I5260d288e766aa17548cdbd27ab2a943f10abcb5

--- app/src/main/java/com/awen/codebase/model/AnnotationReflectModel.java
+++ app/src/main/java/com/awen/codebase/model/AnnotationReflectModel.java
@@ -29,15 +29,15 @@ public class AnnotationReflectModel {
         userInterface.getUser("我之存在，因为有你。");
 
         //反射方式
-        try{
+        try {
             Class c1 = Class.forName("com.awen.codebase.annotition.User");
-            Method method =  c1.getDeclaredMethod("getName");
+            Method method = c1.getDeclaredMethod("getName");
             UserMethod userMethod = method.getAnnotation(UserMethod.class);
             if (userMethod != null) {
                 LogUtil.androidLog("注解方法：" + userMethod.title() + "\n");
             }
 
-            Method method1 =  c1.getDeclaredMethod("print",String.class);//获取单个方法
+            Method method1 = c1.getDeclaredMethod("print", String.class);//获取单个方法
             Annotation[][] parameterAnnotationsArray = method1.getParameterAnnotations();//拿到参数注解
             if (parameterAnnotationsArray != null) {
                 for (int i = 0; i < parameterAnnotationsArray.length; i++) {
@@ -49,11 +49,11 @@ public class AnnotationReflectModel {
                 }
             }
             Method[] methods = c1.getDeclaredMethods();
-            for(Method method2:methods){
+            for (Method method2 : methods) {
 
                 UserMethod userMethod1 = method2.getAnnotation(UserMethod.class);
                 if (userMethod1 != null) {
-                    LogUtil.androidLog(method2.getName()+ "的注解方法：" + userMethod1.title() + "\n");
+                    LogUtil.androidLog(method2.getName() + "的注解方法：" + userMethod1.title() + "\n");
                 }
 
                 Annotation[][] temp = method2.getParameterAnnotations();//拿到参数注解
@@ -62,95 +62,93 @@ public class AnnotationReflectModel {
                         Annotation[] annotations = temp[i];
                         if (annotations != null && annotations.length != 0) {
                             UserParam userParam = (UserParam) annotations[0];
-                            LogUtil.androidLog(method2.getName()+"的注解参数2：" + userParam.name() + "," + userParam.phone() + "\n");
+                            LogUtil.androidLog(method2.getName() + "的注解参数2：" + userParam.name() + "," + userParam.phone() + "\n");
                         }
                     }
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
 
     }
 
     /**
-     *
      * 反射调用常用方法总结
-     *
-     1.实例化Class对象，有三种方式，
-     Class.forName(类名全路径); //通过Class的静态方法
-     对象.getClass() //通过对象.getClass方法
-     int.class //基本数据类型及基本数据类型的封装了，例如Integer
-
-     2.获取父类
-     Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
-     Class<?> superclass = clazz.getSuperclass();
-
-     3.获取实现接口
-     Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
-     Class<?>[] interfaces = clazz.getInterfaces()
-
-     4.获取指定参数构造函数及实例化
-     Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
-     Constructor<?> constructor = clazz.getConstructor(Class<?>  ... class);//获取公共的
-     Constructor<?> constructor = clazz.getDeclaredConstructor()//获取私有的
-     constructor.newInstance(Object args);
-
-     5.获取所有构造函数及构造参数的类型
-     Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
-     Constructor<?>[] constructors = clazz.getConstructors();//公共的
-     Constructor<?>[] constructors = clazz.getDeclaredConstructors()//包括私有的
-
-     for (int i = 0; i < constructors.length; i++) {
-     Class<?> clazzs[] = constructors[i].getParameterTypes();//获取类型
-     System.out.print("constructors[" + i + "] (");
-     for (int j = 0; j < clazzs.length; j++) {
-     if (j == clazzs.length - 1)
-     System.out.print(clazzs[j].getName());
-     else
-     System.out.print(clazzs[j].getName() + ",");
-     }
-     System.out.println(")");
-     }
-
-
-     6.通过无参实例化对象
-     Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
-     class.newInstance();
-
-
-     7.获取字段，修改字段
-     Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
-
-     Field field = clazz.getField(String name);//获取公共字段
-     Field field = clazz.getDeclaredField(String name);//获取私有公共字段
-     Field[] field = clazz.getFields();//获取所有公共字段
-     Field[] field = clazz.getDeclaredFields();//获取包括私有所有字段
-
-     Field field = clazz.getDeclaredField("heihei");
-     field.setAccessible(true);//设置java取消访问检查，也就是说如果是私有的也可以访问,
-     field.set(obj, "Java反射机制");
-
-     8.获取方法,运行方法
-     Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
-
-     clazz.getMethod(String name ,Class<?> ... parame);//获取公共指定方法
-     clazz.getDeclaredMethod(String name ,Class<?> ... parame)//获取私有指定方法
-     clazz.getMethods()//获取公共所有方法
-     clazz.getDeclaredMethods();//获取包括私有全部方法
-
-     Method method = clazz.getMethod("add");
-     method.invoke(clazz.newInstance());
-
-     method = clazz.getMethod("getInfo", int.class, String.class);
-     method.setAccessible(true)//设置java取消访问检查，也就是说如果是私有的也可以访问,
-     method.invoke(clazz.newInstance(), 20, "张三");
-
-
-     9.获取数组或者list中的类型,如果不是数组或集合返回null
-     Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
-     Class<?> componentType = clazz.getComponentType();
-
+     * <p>
+     * 1.实例化Class对象，有三种方式，
+     * Class.forName(类名全路径); //通过Class的静态方法
+     * 对象.getClass() //通过对象.getClass方法
+     * int.class //基本数据类型及基本数据类型的封装了，例如Integer
+     * <p>
+     * 2.获取父类
+     * Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
+     * Class<?> superclass = clazz.getSuperclass();
+     * <p>
+     * 3.获取实现接口
+     * Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
+     * Class<?>[] interfaces = clazz.getInterfaces()
+     * <p>
+     * 4.获取指定参数构造函数及实例化
+     * Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
+     * Constructor<?> constructor = clazz.getConstructor(Class<?>  ... class);//获取公共的
+     * Constructor<?> constructor = clazz.getDeclaredConstructor()//获取私有的
+     * constructor.newInstance(Object args);
+     * <p>
+     * 5.获取所有构造函数及构造参数的类型
+     * Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
+     * Constructor<?>[] constructors = clazz.getConstructors();//公共的
+     * Constructor<?>[] constructors = clazz.getDeclaredConstructors()//包括私有的
+     * <p>
+     * for (int i = 0; i < constructors.length; i++) {
+     * Class<?> clazzs[] = constructors[i].getParameterTypes();//获取类型
+     * System.out.print("constructors[" + i + "] (");
+     * for (int j = 0; j < clazzs.length; j++) {
+     * if (j == clazzs.length - 1)
+     * System.out.print(clazzs[j].getName());
+     * else
+     * System.out.print(clazzs[j].getName() + ",");
+     * }
+     * System.out.println(")");
+     * }
+     * <p>
+     * <p>
+     * 6.通过无参实例化对象
+     * Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
+     * class.newInstance();
+     * <p>
+     * <p>
+     * 7.获取字段，修改字段
+     * Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
+     * <p>
+     * Field field = clazz.getField(String name);//获取公共字段
+     * Field field = clazz.getDeclaredField(String name);//获取私有公共字段
+     * Field[] field = clazz.getFields();//获取所有公共字段
+     * Field[] field = clazz.getDeclaredFields();//获取包括私有所有字段
+     * <p>
+     * Field field = clazz.getDeclaredField("heihei");
+     * field.setAccessible(true);//设置java取消访问检查，也就是说如果是私有的也可以访问,
+     * field.set(obj, "Java反射机制");
+     * <p>
+     * 8.获取方法,运行方法
+     * Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
+     * <p>
+     * clazz.getMethod(String name ,Class<?> ... parame);//获取公共指定方法
+     * clazz.getDeclaredMethod(String name ,Class<?> ... parame)//获取私有指定方法
+     * clazz.getMethods()//获取公共所有方法
+     * clazz.getDeclaredMethods();//获取包括私有全部方法
+     * <p>
+     * Method method = clazz.getMethod("add");
+     * method.invoke(clazz.newInstance());
+     * <p>
+     * method = clazz.getMethod("getInfo", int.class, String.class);
+     * method.setAccessible(true)//设置java取消访问检查，也就是说如果是私有的也可以访问,
+     * method.invoke(clazz.newInstance(), 20, "张三");
+     * <p>
+     * <p>
+     * 9.获取数组或者list中的类型,如果不是数组或集合返回null
+     * Class<?> clazz  = Class.forName(类名全路径); //通过Class的静态方法
+     * Class<?> componentType = clazz.getComponentType();
      */
     public static void invokeReflect() {
         try {
@@ -219,7 +217,6 @@ public class AnnotationReflectModel {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
 
 
     }
