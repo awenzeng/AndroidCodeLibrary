a1e162c09e14d1d71c1a63611c145d479d58b41c
zengfanwen <zengfanwen@coocaa.com>
2021-03-27 11:44:12 +0800

    添加相关注释

--- app/src/main/java/com/awen/codebase/common/ui/LargeImageView.java
+++ app/src/main/java/com/awen/codebase/common/ui/LargeImageView.java
@@ -16,6 +16,23 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
+ * 实现原理：
+ * 读取大图：   InputStream inputStream = getAssets().open("xxxx.jpg");
+ *
+ *             //获得图片的宽、高
+ * 获取大图宽高：BitmapFactory.Options tmpOptions = new BitmapFactory.Options();
+ *             tmpOptions.inJustDecodeBounds = true;
+ *             BitmapFactory.decodeStream(inputStream, null, tmpOptions);
+ *             int width = tmpOptions.outWidth;
+ *             int height = tmpOptions.outHeight;
+ *
+ *             //设置显示图片的中心区域
+ * 区域显示大图  BitmapRegionDecoder bitmapRegionDecoder = BitmapRegionDecoder.newInstance(inputStream, false);
+ *             BitmapFactory.Options options = new BitmapFactory.Options();
+ *             options.inPreferredConfig = Bitmap.Config.RGB_565;
+ *             Bitmap bitmap = bitmapRegionDecoder.decodeRegion(new Rect(width / 2 - 100, height / 2 - 100, width / 2 + 100, height / 2 + 100), options);
+ *             mImageView.setImageBitmap(bitmap)；
+ *
  * Created by zhy on 15/5/16.
  */
 public class LargeImageView extends View {
