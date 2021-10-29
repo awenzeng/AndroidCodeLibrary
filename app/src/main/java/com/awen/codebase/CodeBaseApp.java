12ae010258fe31d5eaa971d62985143436ee772e
zengfanwen <zengfanwen@coocaa.com>
2021-10-29 11:36:23 +0800

    Application中增加主进程判断
    Change-Id: Ia24f8e55f0909a276741fd68b4d3c318357ea3fa

--- app/src/main/java/com/awen/codebase/CodeBaseApp.java
+++ app/src/main/java/com/awen/codebase/CodeBaseApp.java
@@ -1,9 +1,11 @@
 package com.awen.codebase;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Message;
+import android.text.TextUtils;
 
 import com.alibaba.fastjson.JSONObject;
 import com.awen.codebase.activity.ProxyActivity;
@@ -23,6 +25,7 @@ public class CodeBaseApp extends Application {
     public static boolean bUseFont = true;
     public static int SDK_VERSION = android.os.Build.VERSION.SDK_INT;
     public static HttpProxyCacheServer httpProxyCacheServer;
+    private final static String PROCESS_NAME = "com.test";
 
     @Override
     public void onCreate() {
@@ -30,6 +33,10 @@ public class CodeBaseApp extends Application {
         instance = this;
         bUseFont = false;
         AutoScreenUtils.AdjustDensity(this);
+
+        if (isAppMainProcess()) {//主进程
+            //do something for init
+        }
 
 //        //这个ProxyActivity在清单文件中注册过，以后所有的Activitiy都可以用ProxyActivity无需声明，绕过监测
 //        HookAmsUtil hookAmsUtil = new HookAmsUtil(ProxyActivity.class, this);
@@ -53,6 +60,40 @@ public class CodeBaseApp extends Application {
         return instance;
     }
 
+
+    /**
+     * 判断是不是UI主进程，因为有些东西只能在UI主进程初始化
+     */
+    public static boolean isAppMainProcess() {
+        try {
+            int pid = android.os.Process.myPid();
+            String process = getAppNameByPID(getAppContext(), pid);
+            if (TextUtils.isEmpty(process)) {
+                return true;
+            } else if (PROCESS_NAME.equalsIgnoreCase(process)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return true;
+        }
+    }
+
+
+    /**
+     * 根据Pid得到进程名
+     */
+    public static String getAppNameByPID(Context context, int pid) {
+        ActivityManager manager = (ActivityManager)                   context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (android.app.ActivityManager.RunningAppProcessInfo processInfo : manager.getRunningAppProcesses()) {
+            if (processInfo.pid == pid) {
+                return processInfo.processName;
+            }
+        }
+        return "";
+    }
     /**
      * 修改字体
      *
